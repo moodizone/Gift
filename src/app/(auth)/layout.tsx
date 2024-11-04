6be2855cf72fd7cf5d10af77ed2fd4d707a0873c
Moodi <moodizone@gmail.com>
2024-11-04 17:48:20 +0330

    💄 improve spacing

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
   return (
-    <div className="container mx-auto relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container p-3 md:p-0 mx-auto relative h-full flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2">
       <UpperLink />
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
