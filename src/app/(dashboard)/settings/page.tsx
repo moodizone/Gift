0f48d2d6be29d3f3443652dc04e3f45592088282
Moodi <moodizone@gmail.com>
2024-11-04 20:43:06 +0330

    🚧 add settings template

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,12 +1,17 @@
-import { Metadata } from "next";
-import * as React from "react";
+import { Separator } from "@/components/ui/separator"
+import { ProfileForm } from "./profile-form"
 
-export const metadata: Metadata = {
-  title: "Settings",
-};
-
-function page() {
-  return <div>settings goes here</div>;
+export default function SettingsProfilePage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator />
+      <ProfileForm />
+    </div>
+  )
 }
-
-export default page;
