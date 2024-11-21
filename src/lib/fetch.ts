e612b40946a71fa9bc7b32750b5ec436b0960b31
moodi <moodizone@gmail.com>
2024-11-21 13:17:01 +0330

    🧑‍💻 improve clientFetch

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -11,23 +11,19 @@ export async function clientFetch<T = unknown>(
   url: string,
   options?: RequestInit
 ) {
-  try {
-    const newHeaders = new Headers(options?.headers || {});
-    newHeaders.set("Content-Type", "application/json");
-    const newOptions = {
-      ...options,
-      headers: newHeaders,
-    };
-    const base = "http://localhost:3000/api";
-    const response = await fetch(base + url, newOptions);
+  const newHeaders = new Headers(options?.headers || {});
+  newHeaders.set("Content-Type", "application/json");
+  const newOptions = {
+    ...options,
+    headers: newHeaders,
+  };
+  const base = "http://localhost:3000/api";
+  const response = await fetch(base + url, newOptions);
 
-    if (response.ok) {
-      const result = await response.clone().json();
-      return result as T;
-    } else {
-      throw new APIError(response);
-    }
-  } catch (error) {
-    console.error(`🚫 Default client fetch error:\n`, error);
+  if (response.ok) {
+    const result = await response.clone().json();
+    return result as T;
+  } else {
+    throw new APIError(response);
   }
 }
