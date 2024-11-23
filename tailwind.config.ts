a7ddf14d958e7c97cb917a6d03760c347f31d3ae
moodi <moodizone@gmail.com>
2024-11-23 15:51:00 +0330

    🔧 tailwind cover lib directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/hoc/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
