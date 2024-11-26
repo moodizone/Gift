c741e465a2cbaf7874783b5b554065ecc2f570da
moodi <moodizone@gmail.com>
2024-11-26 18:31:17 +0330

    🏷️ update types

--- src/services/type.ts
+++ src/services/type.ts
@@ -23,29 +23,33 @@ export enum language {
 export interface ErrorType {
   message: string | Array<Record<string, string>>;
 }
-
-export interface AuthRegisterBody {
-  email: string;
-  password: string;
-}
-
 export interface AuthLoginBody {
   email: string;
   password: string;
 }
-
 export interface AuthLoginResponse {
   email: string;
   id: number;
   tel: string | null;
   name: string | null;
   gender: gender | null;
-  age: number | null;
+  age: Date | null;
   profilePicture: string | null;
   token: string;
   language: language | null;
 }
-
+export interface AuthRegisterBody {
+  email: string;
+  password: string;
+}
+export type AuthRegisterResponse = AuthLoginResponse;
 export interface AuthEmailAvailabilityBody {
   email: string;
 }
+export interface UserUpdateBody {
+  tel?: string;
+  name?: string;
+  gender?: gender;
+  age?: Date;
+}
+export type UserUpdateResponse = Omit<AuthLoginResponse, "token">;
