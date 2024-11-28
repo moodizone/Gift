d54a4c66e8f57fd96419dfa8d1c21b9e59307f09
moodi <moodizone@gmail.com>
2024-11-28 20:42:40 +0330

    🌐 translate not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,18 @@ import { initI18nInstance } from "@/locale/server-config";
 import Link from "next/link";
 
 export default async function Component() {
-  const i18n = initI18nInstance();
+  const i18n = await initI18nInstance();
   return (
     <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
       <div className="w-full space-y-6 text-center">
         <div className="space-y-3">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
-            {"Oops! Lost in Cyberspace"}
+            {i18n.t("Oops! Lost in Cyberspace")}
           </h1>
           <p className="text-gray-500">
-            {"Looks like you've ventured into the unknown digital realm."}
+            {i18n.t(
+              "Looks like you've ventured into the unknown digital realm."
+            )}
           </p>
         </div>
         <Link
