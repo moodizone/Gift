87d784ac42cb802a065baf3b53a524fcb73d5f10
moodi <moodizone@gmail.com>
2024-12-11 16:16:34 +0330

    🚧 add related utils for sidebar component

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,8 +1,5 @@
 import { Metadata } from "next";
 
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
-
 export const metadata: Metadata = {
   title: "Products",
 };
@@ -12,9 +9,7 @@ export default function MusicPage() {
     <div className="block">
       <div className="bg-background">
         <div className="grid lg:grid-cols-5">
-          <div className="col-span-3 lg:col-span-4 lg:border-l">
-            ss
-          </div>
+          <div className="col-span-3 lg:col-span-4 lg:border-l">ss</div>
         </div>
       </div>
     </div>
