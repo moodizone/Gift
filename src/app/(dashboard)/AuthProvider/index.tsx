0f9b49031f224eb938d3d9ae8425e3ffaab06b29
moodi <moodizone@gmail.com>
2024-12-11 18:12:01 +0330

    🚧 init sidebar to main layout for private routes

--- src/app/(dashboard)/AuthProvider/index.tsx
+++ src/app/(dashboard)/AuthProvider/index.tsx
@@ -3,13 +3,9 @@ import * as React from "react";
 import { serverFetch } from "@/lib/serverFetch";
 import { UserMeResponse } from "@/services/type";
 import Unauthorized from "../../Unauthorized";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Store from "./Store";
+import { AppSidebar } from "./app-sidebar";
 
 async function getMe() {
   const response = await serverFetch<UserMeResponse>("/user/me");
@@ -22,18 +18,11 @@ async function AuthProvider({ children }: React.PropsWithChildren) {
     const me = await getMe();
 
     return (
-      <>
+      <SidebarProvider>
         <Store me={me} />
-        <SidebarProvider open>
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-            </header>
-            {children}
-          </SidebarInset>
-        </SidebarProvider>
-      </>
+        <AppSidebar />
+        <SidebarInset>{children}</SidebarInset>
+      </SidebarProvider>
     );
   } catch {
     // redirect to login page
--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,17 +1,41 @@
-import { Metadata } from "next";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
-export const metadata: Metadata = {
-  title: "Products",
-};
-
-export default function MusicPage() {
+export default function Page() {
   return (
-    <div className="block">
-      <div className="bg-background">
-        <div className="grid lg:grid-cols-5">
-          <div className="col-span-3 lg:col-span-4 lg:border-l">ss</div>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="min-h-[100vh] h-[100vh] overflow-auto flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          Suspense
         </div>
       </div>
-    </div>
+    </>
   );
 }
