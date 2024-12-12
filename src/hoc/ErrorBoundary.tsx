c0f87419a2d88031ae634e2dc92e7dd279287b11
moodi <moodizone@gmail.com>
2024-12-12 18:26:18 +0330

    💄 improve error boundary overflow

--- src/hoc/ErrorBoundary.tsx
+++ src/hoc/ErrorBoundary.tsx
@@ -13,27 +13,25 @@ function ErrorBoundaryProvider({ children }: React.PropsWithChildren) {
     <ErrorBoundary
       fallbackRender={({ error }) => {
         return (
-          <div className="h-full w-full flex justify-center items-center">
-            <div className="max-w-md mx-auto">
-              <Alert className=" flex flex-col">
-                <Terminal className="h-4 w-4" />
-                <AlertTitle>Error!</AlertTitle>
-                <AlertDescription className="flex flex-col gap-4">
-                  <p> {t("Something went wrong, That's all we know!")}</p>
-                  <code className="bg-zinc-200 dark:bg-zinc-900 p-3 rounded">
-                    {error?.message}
-                  </code>
-                  <Button
-                    className="w-full"
-                    onClick={() => {
-                      window.location.reload();
-                    }}
-                  >
-                    {t("reload")}
-                  </Button>
-                </AlertDescription>
-              </Alert>
-            </div>
+          <div className="h-full w-full flex justify-center items-center px-2">
+            <Alert className="max-w-lg mx-auto flex flex-col">
+              <Terminal className="h-4 w-4" />
+              <AlertTitle>Error!</AlertTitle>
+              <AlertDescription className="flex flex-col gap-4">
+                <p> {t("Something went wrong, That's all we know!")}</p>
+                <code className="bg-zinc-200 dark:bg-zinc-900 p-3 rounded break-words">
+                  {error?.message}
+                </code>
+                <Button
+                  className="w-full"
+                  onClick={() => {
+                    window.location.reload();
+                  }}
+                >
+                  {t("reload")}
+                </Button>
+              </AlertDescription>
+            </Alert>
           </div>
         );
       }}
